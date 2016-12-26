be476983f2658ff2604573a7a601679c0129a2cd
d1eggoh <dieggoh@gmail.com>
2016-12-26 15:48:53 -0300

    Initial check-in of module RestCasinoMovil

---
+++ RestCasinoMovil/App_Start/WebApiConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Http;
+
+namespace RestCasinoMovil
+{
+	public static class WebApiConfig
+	{
+		public static void Register(HttpConfiguration config)
+		{
+			// Web API configuration and services
+
+			// Web API routes
+			config.MapHttpAttributeRoutes();
+
+			config.Routes.MapHttpRoute(
+				name: "DefaultApi",
+				routeTemplate: "api/{controller}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			);
+
+			config.Routes.MapHttpRoute(
+			name: "LoginApi",
+				routeTemplate: "api/{controller}/{user}/{pass}/{user_login}/{pass_login}"
+			
+			);
+		}
+	}
+}
---
+++ RestCasinoMovil/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Http;
+
+namespace RestCasinoMovil
+{
+	public class LoginController : ApiController
+	{
+		// GET api/values/5
+		public string Get(int id)
+		{
+			return "value";
+		}
+
+		public string Get(string user, string pass, string user_login, string pass_login)
+		{
+			string salida = "OK";
+			if ((!user.Equals("skpUserName")) || (!pass.Equals("Ghy54,.pfR")))
+			{
+				salida = "ERROR: Credenciales Inválidas";
+			}
+			return salida;
+		}
+
+	}
+}
